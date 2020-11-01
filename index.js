f45d320a0e26493d156e39fdcba24c1bbd3e1450
JossinAntony <jossinde@gmail.com>
2020-11-01 15:12:18 +0100

    all apis- find even or odd, smallest and largest of numbers & divisibilty by 8 implemented.

--- index.js
+++ index.js
@@ -5,13 +5,33 @@ const myops = require('./operations');
 
 const app = express();
 app.use(bodyparser.json());
-app.use
+app.use(bodyparser.urlencoded({extended:true}));
 
 
 app.get('/', (req, res)=>{
-    res.send('Hi');
+    res.send('Welcome..');
 });
 
+app.post('/smallestLargest',(req, res)=>{
+    var getnum1 = parseInt(req.body.num1);
+    var getnum2 = parseInt(req.body.num2);
+    var getnum3 = parseInt(req.body.num3);
+
+    res.status(200).json({'result':myops.findSmallestAndLargest(getnum1, getnum2, getnum3)});
+});
+
+
+app.post('/evenOrOdd', (req, res)=>{
+    var getnum = parseInt(req.body.num);
+    
+    res.status(200).json({'result': myops.evenOrOdd(getnum)});
+});
+
+app.post('/divisibleBy8', (req, res)=>{
+    var getnum = parseInt(req.body.num);
+
+    res.status(200).json({result: myops.divisibilityby8(getnum)});
+});
 
 app.listen(3000, (e)=>{
     if(e){
